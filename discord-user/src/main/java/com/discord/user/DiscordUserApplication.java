bc48b00b66f8b5f246a6ff02c3ff2e69e4cf4def
MrXck <1971877559@qq.com>
2024-09-12 16:01:04 +0800

    first commit

---
+++ discord-user/src/main/java/com/discord/user/DiscordUserApplication.java
@@ -0,0 +1,13 @@
+package com.discord.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordUserApplication.class, args);
+    }
+
+}
